b33f51b12656c072d526c459e8f048bb41e9d5a2
Pradeep Singh <pradeep@veridic.in>
2017-07-18 15:17:00 +0530

    1. Changed the code to:  A. Not use mutable state  B. Fixed the issue of out of order invoices data  C. More functional code now.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,26 +4,36 @@ import scala.collection.mutable.ListBuffer
   *
   */
 object Main {
-  def findMissing(list:List[InvoiceData]) = {
-    var missingInvoices:ListBuffer[(Int, List[Int])] = ListBuffer()
-    var first = list.head
-    for(next<-list.tail){
-      if(first.endInvoiceNumber+1!=next.startInvoiceNumber&&first.category==next.category){
-        val tuple = (first.category, (first.endInvoiceNumber+1 until next.startInvoiceNumber).toList)
-        missingInvoices+=tuple
+
+  def findMissing(list: List[Int]) = {
+    def findMissingR(first:Int, remaining:List[Int], acc:List[Int]):List[Int] = remaining match{
+      case Nil=> Nil
+      case first::last::Nil=>{
+        if(first+1!=last) (first+1 until last).toList:::acc
+        else acc
       }
-      first = next
+      case head::tail=>{
+        if(first+1!=head){
+          val missing = (first+1 until head).toList
+          findMissingR(head, tail, missing:::acc)
+        }
+        else
+          findMissingR(head, tail, acc)
+      }
     }
-    missingInvoices.map(x=>x._1).distinct.map(c=>(c, missingInvoices.filter(i=>i._1==c).flatMap(z=>z._2)))
+    if(list.length<=0) List[Int]()
+    else{
+      findMissingR(list.head, list.tail, List[Int]())
+    }
   }
 
   def main(args:Array[String]):Unit={
-    val invoiceList = List(InvoiceData(1,"A", 101, 104),InvoiceData(1,"B", 106, 107), InvoiceData(2,"A", 201, 205), InvoiceData(2,"B", 208, 210),InvoiceData(1,"C", 109, 113))
-    val sorted = invoiceList.sortBy(x=>(x.category, x.startInvoiceNumber))
-    println(sorted)
-    val missing = findMissing(sorted)
-    for(item<-missing){
-      println(s"Category: ${item._1}\t\tInvoices: ${item._2.mkString(",")}")
-    }
+    val invoiceList = List(InvoiceData(1,"A", 101, 104),InvoiceData(1,"B", 106, 107), InvoiceData(2,"A", 201, 205), InvoiceData(2,"B", 208, 210),InvoiceData(1,"C", 109, 113), InvoiceData(1, "D", 105, 108))
+    val categoryInvoices = invoiceList.map(x=>(x.category, x.productId, (x.startInvoiceNumber to x.endInvoiceNumber).toList))
+     .flatMap(x=>List((x._1, x._3)))
+    val categoryInvoicesFlat =  categoryInvoices.map(c=>c._1).distinct.map(x=>(x, categoryInvoices.filter(y=>y._1==x).map(z=>z._2).flatten.distinct.sorted))
+    val missingInvoices = categoryInvoicesFlat.map(x=>(x._1, findMissing(x._2))).filter(x=>x._2.length>0)
+    for(item<-missingInvoices)
+      println(s"Category: ${item._1}\t\t Missing: ${item._2}")
   }
 }
